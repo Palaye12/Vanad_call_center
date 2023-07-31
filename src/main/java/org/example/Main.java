2c93b95b7cf9e224c371be87e349f0e4f979b29d
Palaye12 <palayesambinfo@gmail.com>
2023-07-31 19:34:40 +0000

    integration des fonctions et test (serveur et types de services )

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.tutorial.SimulateOneDay;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //System.out.println("Hello world!");
+
+        try {
+            // Appel de la fonction createDayCustomers depuis SimulateOneday
+            SimulateOneDay simulate = new SimulateOneDay();
+            simulate.createDayCustomers("Dataset/janvier/2014-01-02.csv");
+            simulate.displayFileContent();
+            simulate.displayNbAgentsList();
+            simulate.displayArrivalTimes(); // Afficher les temps d'arrivée des clients
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/org/example/tutorial/SimulateOneDay.java
+++ src/main/java/org/example/tutorial/SimulateOneDay.java
@@ -5,26 +5,23 @@ import umontreal.ssj.simevents.Event;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 
 public class SimulateOneDay {
 
-    double[] LES = new double[27];
+    private List<Double> arrivalTimes = new ArrayList<>();
+
+    HashMap<Integer, Double> LES = new HashMap<Integer, Double>();
     LinkedList<Customer> waitList = new LinkedList<>();
     int nbServeurs = 0 ;
-    int[] qLengthRealTime = new int[27];
+    HashMap<Integer,Integer> qLengthRealTime = new HashMap<Integer,Integer>();
+    HashMap<Integer,HashSet<Double> > nbagentsList = new HashMap<>();
 
     ArrayList<Customer> servedCustomers = new ArrayList<>();
-    public int getCase(int type){
-        if (type == 30175){
-            return 0;
-        }
-        // A compléter pour les 27 types de services
-        return 1;
-    }
+
 
     class Customer {
+        public Double agentNumber;
         int type;
         double arrivalTime;
         double les;
@@ -32,20 +29,43 @@ public class SimulateOneDay {
         double waitingTime;
         int qLength;
         // Savoir la case qui est réservé à chaque type
-        int[] qLengthAtArrival = new int[27];
+        HashMap<Integer,Integer> qLengthAtArrival = new HashMap<Integer,Integer>();
         double serviceTime ;
         boolean abandon = false;
+
     }
 
     public void createDayCustomers(String dayFile) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader(dayFile));
-        String readedLine = br.readLine();
+        //lis l'entête du fichier
+        String headerLine = br.readLine();
+        String readedLine ;
+        Integer type;
+        Double agent_number;
 
-        while(readedLine != null){
+
+        while((readedLine= br.readLine()) != null){
             Customer customer = new Customer();
-            customer.type = Integer.parseInt(readedLine.split(",")[1]);
+            type = Integer.parseInt(readedLine.split(",")[1]);
+            if(qLengthRealTime.isEmpty() || qLengthRealTime.containsKey(type)==false ){
+                qLengthRealTime.put(type,0);
+            }
+            String agentNumberString = readedLine.split(",")[2];
+            if (!agentNumberString.isEmpty() && !agentNumberString.equals("NULL")) {
+                agent_number = Double.parseDouble(agentNumberString);
+                if (nbagentsList.containsKey(type)) {
+                    nbagentsList.get(type).add(agent_number);
+                } else {
+                    HashSet<Double> nbAgents = new HashSet<>();
+                    nbAgents.add(agent_number);
+                    nbagentsList.put(type, nbAgents);
+                }
+            }
+            customer.type = type;
             customer.arrivalTime = getTime(readedLine.split(",")[0]);
+
+            arrivalTimes.add(customer.arrivalTime); // Ajouter le temps d'arrivée du client à la liste
 
             if (readedLine.split(",")[3] != "NULL"){
                 customer.serviceTime = getTime(readedLine.split(",")[6]) - getTime(readedLine.split(",")[3]);
@@ -58,11 +78,43 @@ public class SimulateOneDay {
             new ArrivalInQueue(customer).schedule(getTime(readedLine.split(",")[0]));
         }
     }
-    public double getTime(String s){
-        String s1 = s.split(" ")[1];
-        return Integer.parseInt(s1.split(":")[0])*3600
-                +Integer.parseInt(s1.split(":")[1])*60
-                +Integer.parseInt(s1.split(":")[2])-(8*3600);
+    public void displayArrivalTimes() {
+        System.out.println("Temps d'arrivée des clients :");
+        for (int i = 0; i < arrivalTimes.size(); i++) {
+            double arrivalTime = arrivalTimes.get(i);
+            System.out.println("Client " + (i + 1) + " : " + arrivalTime + " secondes");
+        }
+    }
+
+    public double getTime(String s) {
+        String[] sParts = s.split(" ");
+        if (sParts.length >= 2) {
+            String s1 = sParts[1];
+            return Double.parseDouble(s1.split(":")[0]) * 3600
+                    + Double.parseDouble(s1.split(":")[1]) * 60
+                    + Double.parseDouble(s1.split(":")[2]) - (8 * 3600);
+        } else {
+            // Gérer l'erreur ou retourner une valeur par défaut si nécessaire
+            return 0.0;
+        }
+    }
+
+    public void displayNbAgentsList() {
+        System.out.println("Nombre de serveurs pour chaque type :");
+        for (Integer type : nbagentsList.keySet()) {
+            HashSet<Double> nbAgents = nbagentsList.get(type);
+            System.out.println("Type " + type + " : " + nbAgents.size() + " serveur(s)");
+        }
+    }
+
+
+    public void displayFileContent() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("Dataset/janvier/2014-01-02.csv"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
     }
 
     class ArrivalInQueue extends Event {
@@ -74,16 +126,20 @@ public class SimulateOneDay {
 
         @Override
         public void actions() {
-            customer.les = LES[getCase(customer.type)];
-            customer.qLength = qLengthRealTime[getCase(customer.type)];
+            Integer length = qLengthRealTime.get(customer.type);
+            customer.les = LES.get(customer.type);
+            customer.qLength = length ;
 
-            for (int i = 0 ; i < 27; i++){
-                customer.qLengthAtArrival[i] = qLengthRealTime[i];
+            for (Integer i :qLengthRealTime.keySet()){
+                customer.qLengthAtArrival.put(i,qLengthRealTime.get(i)) ;
             }
             // Initialiser le nombre de serveurs disponibles (A faire)
-
+            customer.nbServeurs= nbagentsList.get(customer.type).size();
             waitList.add(customer);
-            qLengthRealTime[getCase(customer.type)] += 1;
+            // mise à jour de la longueur de la file d'attente
+            qLengthRealTime.put(customer.type,length+1);
+
+
             new Departure(customer).schedule(customer.waitingTime);
         }
     }
@@ -96,13 +152,17 @@ public class SimulateOneDay {
 
         @Override
         public void actions() {
+            Integer length = qLengthRealTime.get(customer.type);
             waitList.remove(customer);
-            qLengthRealTime[getCase(customer.type)] -= 1;
+            qLengthRealTime.put(customer.type,length-1);
 
             if (!customer.abandon){
-                LES[getCase(customer.type)] = customer.waitingTime;
+                LES.put(customer.type, customer.waitingTime);
                 servedCustomers.add(customer);
+
+
             }
+
         }
     }
 }
